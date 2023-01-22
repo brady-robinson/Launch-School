9804d9dc7bd4fe15332af71ec608fd020e171b34
Brady Robinson <bradyrobinson3@gmail.com>
2023-01-22 09:40:08 -0500

    add file

--- JS101/lesson2/variables_as_pointers.js
+++ JS101/lesson2/variables_as_pointers.js
@@ -1,1 +1,41 @@
-console.log('hello');
+// let myWord = 'Hello';
+// let myOtherWord = myWord;
+
+// console.log(myWord);
+// console.log(myOtherWord); // different strings stored at different memory addresses
+
+// let myWord = 'Hello';
+// let myOtherWord = myWord;
+// myWord = 'Goodbye';
+
+// console.log(myWord);
+// console.log(myOtherWord);
+
+// let myWords = ['Hello', 'Goodbye'];
+// let myOtherWords = myWords;
+// myWords[0] = 'Hi';
+
+// console.log(myWords);
+// console.log(myOtherWords);
+
+// let myWords = ['Hello', 'Goodbye'];
+// let myOtherWords = myWords;
+// myWords = ['Hi', 'Bye'];
+
+// console.log(myWords);
+// console.log(myOtherWords);
+
+// let myWords = ['Hello', 'Goodbye'];
+// let myWord = myWords[0];
+// myWords[0] = 'Hi';
+
+// console.log(myWords);
+// console.log(myWord);
+
+let myWords = ['Hello', 'Goodbye'];
+let myWord = 'Hi';
+myWords[0] = myWord;
+myWord = 'Hello';
+
+console.log(myWords);
+console.log(myWord);
