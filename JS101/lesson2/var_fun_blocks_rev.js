235e3b756b79c5ee2d87378d6fae755b053d970d
Brady Robinson <bradyrobinson3@gmail.com>
2023-01-23 18:56:10 -0500

    update file

---
+++ JS101/lesson2/var_fun_blocks_rev.js
@@ -0,0 +1,81 @@
+// let myVar = 1;
+
+// function myFunc() {
+//   myVar = 2;
+// }
+
+// myFunc();
+// console.log(myVar);
+
+// let myVar = [1];
+
+// function myFunc() {
+//   myVar = [2];
+// }
+
+// myFunc();
+// console.log(myVar);
+
+// let myVar = [1];
+
+// function myFunc() {
+//   myVar[0] = 2;
+// }
+
+// myFunc();
+// console.log(myVar)
+
+// let myVar = 1;
+
+// function myFunc(myVar) {
+//   myVar = 2;
+// }
+
+// myFunc();
+// console.log(myVar);
+
+// let myVar = [1];
+
+// function myFunc(myVar) {
+//   myVar = [2];
+// }
+
+// myFunc();
+// console.log(myVar)
+
+// let myVar = [1];
+
+// function myFunc(myVar) {
+//   myVar[0] = 2;
+// }
+
+// myFunc();
+// console.log(myVar)
+
+// let myVar = 1;
+
+// function myFunc(myVar) {
+//   myVar = 2;
+// }
+
+// myFunc(myVar);
+// console.log(myVar);
+
+// let myVar = [1];
+
+// function myFunc(myVar) {
+//   myVar = [2];
+// }
+
+// myFunc(myVar);
+// console.log(myVar)
+
+let myVar = [1];
+
+function myFunc(myVar) {
+  myVar[0] = 2;
+}
+
+myFunc(myVar);
+console.log(myVar);
+
