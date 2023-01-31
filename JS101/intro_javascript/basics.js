8944853481dd5aba030115b6d51ef44c04e38941
Brady Robinson <bradyrobinson3@gmail.com>
2023-01-31 05:39:09 -0500

    add file

---
+++ JS101/intro_javascript/basics.js
@@ -0,0 +1,17 @@
+// function p(input) {
+//   console.log(input)
+// }
+
+// "He said, 'Hi there!'"
+// 'He said, \'Hi there!\''
+// 'He said, "Hi there!"'
+// 'He said, \"Hi there!\"'
+
+// p(`5 plus 5 is ${5 + 5}`)
+// p(`blah ${23423}`)
+
+p(typeof 5);
+p(typeof 'hello');
+p(typeof null);
+p(typeof undefined);
+p(typeof true);
