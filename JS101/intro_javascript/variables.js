9587b5a90c11b20323593cca02ff43d6e0a39f14
Brady Robinson <bradyrobinson3@gmail.com>
2023-02-01 05:09:47 -0500

    add files

---
+++ JS101/intro_javascript/variables.js
@@ -0,0 +1,26 @@
+// let a = 4;
+// let b = a;
+// a = 7;
+// console.log(b)
+
+// let a = 'foo';
+
+// if (1===1) {
+//   a = 'bar';
+// }
+
+// console.log(a);
+
+// {
+//   let foo = 'bar';
+// }
+
+// console.log(foo);
+
+const FOO = 'bar';
+
+{
+  const FOO = 'qux';
+}
+
+console.log(FOO);
