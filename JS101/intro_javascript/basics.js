262eee0eca2db733f764f1522cad5afd96174157
Brady Robinson <bradyrobinson3@gmail.com>
2023-01-31 21:15:29 -0500

    add files

--- JS101/intro_javascript/basics.js
+++ JS101/intro_javascript/basics.js
@@ -1,6 +1,6 @@
-// function p(input) {
-//   console.log(input)
-// }
+function p(input) {
+  console.log(input)
+}
 
 // "He said, 'Hi there!'"
 // 'He said, \'Hi there!\''
@@ -10,8 +10,25 @@
 // p(`5 plus 5 is ${5 + 5}`)
 // p(`blah ${23423}`)
 
-p(typeof 5);
-p(typeof 'hello');
-p(typeof null);
-p(typeof undefined);
-p(typeof true);
+// p(typeof 5);
+// p(typeof 'hello');
+// p(typeof null);
+// p(typeof undefined);
+// p(typeof true);
+// p(typeof [1, 2, 3]);
+
+// primitive values: Number, Boolean, String, Null, Undefined
+
+// p(typeof NaN)
+// p(typeof Infinity)
+// p(typeof -Infinity)
+
+// console.log("Brady" + " Robinson")
+// console.log(4936 % 10)
+// console.log(493 % 10)
+// console.log(49 % 10)
+// console.log(4 % 10)
+
+// p(typeof undefined)
+// console.log(Number('5') + 10);
+// p(`The value of 5 + 10 is ${Number('5') + 10}`)
