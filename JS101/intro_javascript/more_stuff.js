abcdd9ae4eb036551b1bf081549d4a3631683300
Brady Robinson <bradyrobinson3@gmail.com>
2023-02-13 05:07:48 -0500

    add file

---
+++ JS101/intro_javascript/more_stuff.js
@@ -0,0 +1,63 @@
+// let count = 1;
+// count = 2;
+
+// let a = 5;
+// let b = a;
+// a = 8;
+// console.log(a);
+// console.log(b);
+
+// let obj = { a: 1 };
+// obj = { b: 2 };
+
+// let c = [1, 2];
+// let d = c;
+// c = [3, 4];
+// console.log(c);
+// console.log(d);
+
+// let e = [1, 2];
+// let f = e;
+// e.push(3, 4);
+// console.log(e);
+// console.log(f);
+
+
+// let g = ['a', 'b', 'c'];
+// let h = g;
+// g[1] = 'x';
+// console.log(g);
+// console.log(h);
+
+// let obj = { foo: 1, bar: 2, qux: 'c' };
+
+// for (let key in obj) {
+//   console.log(key);
+// }
+
+// let arr = [1, 2, 3];
+
+// for (let arrItem in arr) {
+//   console.log(arrItem);
+// }
+
+// for (let arrItem of arr) {
+//   console.log(arrItem);
+// }
+
+// let testString = 'abcde';
+
+// for (let letter of testString) {
+//   console.log(letter);
+// }
+
+
+console.log(/o/.test('bobcat'));
+console.log(/l/.test('bobcat'));
+
+console.log("bobcat".match(/x/));
+console.log("bobcat".match(/[bct]/g));
+console.log("bobcat".match(/b((o)b)/));
+
+
+
--- JS101/intro_javascript/objects.js
+++ JS101/intro_javascript/objects.js
@@ -36,22 +36,143 @@
 // console.log(studentBob.name);
 
 
-let person = {
-  name:    'Jane',
-  age:     37,
-  hobbies: ['photography', 'genealogy'],
-};
+// let person = {
+//   name:    'Jane',
+//   age:     37,
+//   hobbies: ['photography', 'genealogy'],
+// };
 
 
-for (let prop in person) {
-  console.log(person[prop]);
+// for (let prop in person) {
+//   console.log(person[prop]);
+// }
+
+
+// let obj1 = {a: 1, b: 2};
+// let obj2 = Object.create(obj1);
+// obj2.c = 3;
+// obj2.d = 4;
+
+// for (let prop in obj2) {
+//   if (obj2.hasOwnProperty(prop)) {
+//     console.log(prop)
+//   }
+// }
+
+// let obj1 = {a: 1, b: 2};
+// let obj1Keys = Object.keys(obj1);
+
+// obj1Keys.forEach(function(key) {
+//   console.log(obj1[key]);
+// })
+
+// let obj1Values = Object.values(obj1);
+// console.log(obj1Values);
+
+// let obj1Entries = Object.entries(obj1);
+// console.log(obj1Entries);
+
+
+// let obj1 = {a: 1, b: 2};
+// let obj2 = {c: 3, d: 4};
+// let obj3 = Object.assign({}, obj1, obj2);
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj3);
+
+
+// let person = {
+//   name:       'Bob',
+//   occupation: 'web developer',
+//   hobbies:    'painting',
+// };
+
+// console.log(person.name);
+
+
+// let obj = {
+//   b: 2,
+//   a: 1,
+//   c: 3,
+// };
+
+// let objKeys = Object.keys(obj);
+
+// for (let i = 0; i < objKeys.length; i += 1) {
+//   objKeys[i] = objKeys[i].toUpperCase();
+// }
+
+// console.log(objKeys);
+
+
+// let myProtoObj = {
+//   foo: 1,
+//   bar: 2,
+// };
+
+// let myObj = Object.create(myProtoObj);
+// myObj.qux = 3;
+
+// function copyObj(objToCopy, elements=[]) {
+//   if (elements.length === 0) {
+//     return Object.assign({}, objToCopy)
+//   };
+
+//   let outputObj = {};
+//   elements.forEach(key => {
+//     outputObj[key] = objToCopy[key];
+//   });
+
+//   return outputObj;
+// }
+
+// let objToCopy = {
+//   foo: 1,
+//   bar: 2,
+//   qux: 3,
+// };
+
+// let newObj = copyObj(objToCopy);
+// console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
+
+// let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
+// console.log(newObj2);       // => { foo: 1, qux: 3 }
+
+// let newObj3 = copyObj(objToCopy, [ 'bar' ]);
+// console.log(newObj3);       // => { bar: 2 }
+
+
+// let obj = {
+//   foo: { a: "hello", b: "world" },
+//   bar: ["example", "mem", null, { xyz: 6 }, 88],
+//   qux: [4, 8, 12]
+// };
+
+// obj.bar[3].xyz = 606;
+// console.log(obj);
+
+
+// function foo(bar) {
+//   console.log(bar, bar, bar);
+// }
+
+// let bar = foo;
+
+// foo("hello"); // should print "hello hello hello"
+// bar("hi");    // should print "hi hi hi"
+
+let messages = ['welcome', 3.14, [1, 2, 3]];
+function bar() {
+  let current_message = messages.shift();
+  return console.log(current_message);
+} 
+
+function foo(bar) {
+  console.log(bar());
 }
 
-
-
-
-
-
-
+foo(bar);    // Should print 'Welcome'
+foo(bar);    // Should print 3.1415
+foo(bar);    // Should print [1, 2, 3]
 
 
