174ddf6d0eda58650c5d394402ce3aecaf49ec22
Brady Robinson <bradyrobinson3@gmail.com>
2023-02-19 17:40:15 -0500

    add file

---
+++ JS101/lesson3/second_time/easy1.js
@@ -0,0 +1,51 @@
+// let numbers = [1, 2, 3];
+// numbers[6] = 5;
+// console.log(numbers[4]);
+// console.log(numbers);
+
+// let str1 = "Come over here!"; // true
+// let str2 = "What's up, Doc?"; // false
+
+// console.log(str1.includes("!"));
+// console.log(str2.includes("!"));
+
+// let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+// console.log(Object.keys(ages).includes("Spot"));
+// console.log(ages.hasOwnProperty("Spot"));
+
+// let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// let lowerMunsters = munstersDescription[0].toUpperCase() + 
+//                     munstersDescription.toLowerCase().slice(1)
+
+// let lowerMunsters2 = munstersDescription.charAt(0).toUpperCase() +
+//                      munstersDescription.substring(1).toLowerCase()
+
+// console.log(lowerMunsters2)
+
+// let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+// let additionalAges = { Marilyn: 22, Spot: 237 };
+
+// Object.assign(ages, additionalAges);
+// console.log(ages);
+
+// let str1 = "Few things in life are as important as house training your pet dinosaur.";
+// let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+// console.log(str1.includes("Dino"));
+// console.log(str2.includes("Dino"));
+
+// let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+// flintstones.push("Dino", "Hoppy");
+// console.log(flintstones);
+
+// let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// console.log(advice.substring(0,advice.indexOf("house")));
+
+
+
+
+
+
+
+
