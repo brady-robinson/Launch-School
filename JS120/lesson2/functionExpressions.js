dd87f6bdcd9b7720745965c620557c1afd22271d
Brady Robinson <bradyrobinson3@gmail.com>
2023-05-03 04:11:36 -0400

    add file

---
+++ JS120/lesson2/functionExpressions.js
@@ -0,0 +1,56 @@
+// prompt("How are you today?")
+
+// function prompt(message) {
+//   console.log(`=> ${message}`);
+// }
+
+
+// bar();
+// function bar() {
+//   console.log("this is bar");
+// }
+
+// foo();
+// const foo = function() {
+//   console.log("this is foo");
+// }
+
+
+
+// let squaredNums = [1, 2, 3].map(function(num) {
+//   return num * num;
+// });
+
+// console.log(squaredNums);
+
+// let squaredNums = [1, 2, 3].map(function squaredNums(num) {
+//   return num * num;
+// });
+
+// console.log(squaredNums);
+
+
+// let foo = function bar() {};
+// foo();
+// bar();
+
+
+// function say(words) {
+//   console.log(words);
+// }
+
+// let speak = say;
+
+// speak("Hey!");
+
+
+// function logNum(num) {
+//   console.log('Number: ' + num);
+// }
+
+// [1, 2, 3].forEach(logNum);
+
+let myFunc = function() {};
+console.log(typeof myFunc);
+
+
