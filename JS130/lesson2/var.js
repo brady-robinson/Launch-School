bc8acc4a9c5b20589f7ae9f71e1c7e3aed93d5df
Brady Robinson <bradyrobinson3@gmail.com>
2023-08-11 08:19:22 -0400

    add file

---
+++ JS130/lesson2/var.js
@@ -0,0 +1,29 @@
+// var foo;
+// var bar = "qux";
+// var baz = 3.1415;
+
+// let foo;
+// let bar = 'qux';
+// const baz = 3.1415;
+
+// function foo() {
+//   if (true) {
+//     var a = 1;
+//     let b = 2;
+//   }
+
+//   console.log(a);
+//   console.log(b);
+// }
+
+// foo();
+
+function foo() {
+  if (false) {
+    var a = 1;
+  }
+
+  console.log(a);
+}
+
+foo();
