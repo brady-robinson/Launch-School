772d2e55df0ffa1d5de77d2abb2d626dea992639
Brady Robinson <bradyrobinson3@gmail.com>
2023-08-25 10:49:16 -0400

    add file

---
+++ JS130/lesson2/main.js
@@ -0,0 +1,13 @@
+import { foo as renamedFoo } from "./foo";
+renamedFoo();
+
+// const { logIt, setPrefix } = require("./modules");
+// logIt("You rock!")
+// setPrefix("++ ")
+// logIt("You rock!")
+
+// console.log(module);
+// console.log(exports);
+// console.log(require);
+// console.log(__dirname);
+// console.log(__filename);
